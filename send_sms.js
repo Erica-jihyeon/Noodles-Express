2730fd1a4948c0e8a129d6ca4b46913a7f3cccc1
samsam3268 <samskleung@gmail.com>
2022-01-08 12:55:38 -0800

    simpliflied twilioFromPhone param

--- send_sms.js
+++ send_sms.js
@@ -6,7 +6,14 @@ require('dotenv').config();
 //npm install twilio
 //npm install dotenv
 
-const send_sms = function(message, fromPhone, toPhone) {
+//Use the following line to call this function
+//const { send_sms } = require('./send_sms');
+//param1 -> message = string, the message you want to send
+//param2 -> toPhone = string, must be 10 digit, no need +1 , phone number you want to send to
+//e.g. send_sms("This is Noodles Express, +17781234567")
+
+
+const send_sms = function(message, toPhone) {
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const twilioFromPhone = process.env.TWILIO_FROM_NUMBER;
@@ -16,12 +23,12 @@ const send_sms = function(message, fromPhone, toPhone) {
   client.messages
     .create({
       body: `${message}`,
-      from: `+${twilioFromPhone}`,
-      to: `${toPhone}`
+      from: `+1${twilioFromPhone}`,
+      to: `+1${toPhone}`
     })
     .then(message => {
       console.log(message.sid)
-      console.log(`Message sent to : ${message.to} content : ${message.body}`)
+      console.log(`Message sent to : ${message.to} | Content : ${message.body}`)
     });
 }
 
