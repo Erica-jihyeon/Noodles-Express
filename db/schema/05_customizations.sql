cbfb5ed7abd2fa4a8f5e6c2901613ee524a3657f
Brenon <brenonparry@gmail.com>
2022-01-08 19:12:14 +0000

    add NOT NULL to last three fields

--- db/schema/05_customizations.sql
+++ db/schema/05_customizations.sql
@@ -1,11 +1,9 @@
--- Drop and recreate Widgets table (Example)
-
 DROP TABLE IF EXISTS customizations CASCADE;
 
 CREATE TABLE customizations (
   id SERIAL PRIMARY KEY NOT NULL,
   menu_item_id INTEGER REFERENCES menu_items(id),
-  spiciness INTEGER,
-  hot BOOLEAN,
-  item_size VARCHAR(10),
+  spiciness INTEGER NOT NULL,
+  hot BOOLEAN NOT NULL,
+  item_size VARCHAR(10) NOT NULL
 );
