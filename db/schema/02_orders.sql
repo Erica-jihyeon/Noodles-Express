70eb04a8ac29364f673541b9cbc9b06cd671d248
Brenon <brenonparry@gmail.com>
2022-01-08 18:46:16 +0000

    create orders table for Noodles Express DB

---
+++ db/schema/02_orders.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  order_time TIMESTAMP NOT NULL,
+  pick_up_time TIMESTAMP NOT NULL,
+  order_status VARCHAR(255)
+);
