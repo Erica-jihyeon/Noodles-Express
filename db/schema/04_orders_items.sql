f87bb58459ad19949855ed05be2a3307f28ba721
Brenon <brenonparry@gmail.com>
2022-01-08 19:11:44 +0000

    remove comma from menu_item_id field

--- db/schema/04_orders_items.sql
+++ db/schema/04_orders_items.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS orders_items CASCADE;
 CREATE TABLE orders_items (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER REFERENCES orders(id),
-  menu_item_id INTEGER REFERENCES menu_items(id),
+  menu_item_id INTEGER REFERENCES menu_items(id)
 );
