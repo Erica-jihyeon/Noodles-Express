d1720fa1834d4b677e6dfce00efa47eaa370dff2
Brenon <brenonparry@gmail.com>
2022-01-09 00:38:54 +0000

    create seeds for customizations

---
+++ db/seeds/05_customizations.sql
@@ -0,0 +1,8 @@
+INSERT INTO orders (spiciness, hot, item_size, menu_item_id) VALUES (5, true, 'large', 1),
+INSERT INTO orders (spiciness, hot, item_size, menu_item_id) VALUES (3, true, 'small', 6),
+INSERT INTO orders (spiciness, hot, item_size, menu_item_id) VALUES (0, false, 'large', 4),
+INSERT INTO orders (spiciness, hot, item_size, menu_item_id) VALUES (3, true, 'small', 2),
+INSERT INTO orders (spiciness, hot, item_size, menu_item_id) VALUES (0, false, 'med', 5),
+INSERT INTO orders (spiciness, hot, item_size, menu_item_id) VALUES (2, true, 'large', 3);
+
+
