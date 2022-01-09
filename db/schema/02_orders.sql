8651f8cbbcdba077e3f6acaea9df8fcb623c4cdc
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-09 22:03:08 +0000

    edited ERD and tables' schema

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -3,7 +3,11 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  order_time TIMESTAMP NOT NULL,
+  order_time TIMESTAMP,
   pick_up_time TIMESTAMP,
   order_status VARCHAR(255)
 );
+
+--order_time is null -> itmes in cart
+--order_time is not null & pick_up_time is null -> not completed
+--order_time is not null & pick_up_time is not null -> completed
