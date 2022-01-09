5652b3cb6eff62f92c7c7deccc9d3c1b37767be3
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-09 02:53:36 +0000

    fixed db tables create and insert problems

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  order_time DATETIME NOT NULL,
-  pick_up_time DATETIME NOT NULL,
+  order_time TIMESTAMP NOT NULL,
+  pick_up_time TIMESTAMP NOT NULL,
   order_status VARCHAR(255)
 );
--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,23 +1,23 @@
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('pork ramen', '12', 'Chashu, Black Fungus, Egg, Green Onion / Chashu (porc braisé), Champignons Noirs, Œuf, Oignons Verts', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641667836/noodles-express/ramen_uk5h1o.jpg'
+VALUES ('pork ramen', '12', 'Chashu, Black Fungus, Egg, Green Onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641667836/noodles-express/ramen_uk5h1o.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641667836/noodles-express/ramen_uk5h1o.jpg', 'main');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('vegetable ramen', '10', 'Vegetarian spicy mixed noodle with eggplant peperoncini, croquette, bamboo shoot and green onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671803/noodles-express/vegetable_ramen_da4nmo.jpg'
+VALUES ('vegetable ramen', '10', 'Vegetarian spicy mixed noodle with eggplant peperoncini, croquette, bamboo shoot and green onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671803/noodles-express/vegetable_ramen_da4nmo.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641671803/noodles-express/vegetable_ramen_da4nmo.jpg', 'main');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('mushroom fried noodle', '12', 'Freshly Made Hand pulled noodles, mushrooms, onion and carrots', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641671802/noodles-express/mushroom_fried_noodle_zuduei.jpg'
+VALUES ('mushroom fried noodle', '12', 'Freshly Made Hand pulled noodles, mushrooms, onion and carrots', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641671802/noodles-express/mushroom_fried_noodle_zuduei.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_200/v1641671802/noodles-express/mushroom_fried_noodle_zuduei.jpg', 'main');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('coke', '3', 'Coke with ice', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671780/noodles-express/coke_hpel5p.jpg'
+VALUES ('coke', '3', 'Coke with ice', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671780/noodles-express/coke_hpel5p.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641671780/noodles-express/coke_hpel5p.jpg', 'drink');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('strawberry_crepes', '5', 'Fresh Strawberries and whipping cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641672491/noodles-express/strawberry_crepes_b6safy.jpg'
+VALUES ('strawberry_crepes', '5', 'Fresh Strawberries and whipping cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641672491/noodles-express/strawberry_crepes_b6safy.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641672491/noodles-express/strawberry_crepes_b6safy.jpg', 'dessert');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('fried rolls', '8', 'Fresh fried rolls, cheese and onions', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641673046/noodles-express/fried_roll_to2sbh.jpg'
+VALUES ('fried rolls', '8', 'Fresh fried rolls, cheese and onions', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641673046/noodles-express/fried_roll_to2sbh.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641673046/noodles-express/fried_roll_to2sbh.jpg', 'appetizer');
