ac2d5c6d6a0d73d746fb60f4c090999c4fd51fdf
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-09 22:03:45 +0000

    add more data for test

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -4,4 +4,12 @@ INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (3, 
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (4, '2021-12-04T08:00:00.000Z', '2021-12-04T09:00:00.000Z', 'complete');
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (5, '2021-12-05T08:00:00.000Z', '2021-12-05T09:00:00.000Z', 'complete');
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (6, '2021-12-06T08:00:00.000Z', '2021-12-06T09:00:00.000Z', 'complete');
+
+---- test set ---
+--history
+INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2021-12-09T08:00:00.000Z', '2021-12-09T09:00:00.000Z', 'complete');
+INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2021-12-10T08:00:00.000Z', '2021-12-10T09:00:00.000Z', 'complete');
+--current order
 INSERT INTO orders (user_id, order_time,order_status) VALUES (7, '2022-01-09T08:00:00.000Z', 'Preparing your meal');
+--cart
+INSERT INTO orders (user_id) VALUES (7);
---
+++ db/seeds/04_customizations.sql
@@ -0,0 +1,22 @@
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (5, true, 'large', 1, 1);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (3, true, 'small', 1, 2);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (1, false, 'large', 2, 2);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (3, true, 'small', 2, 3);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (1, false, 'med', 2, 4);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (2, true, 'large', 3, 2);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (2, true, 'large', 3, 4);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (2, true, 'large', 3, 6);
+------ test set ---
+-- history 1
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (2, true, 'large', 7, 4);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (3, true, 'med', 7, 5);
+-- history 2
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (4, true, 'small', 8, 2);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (1, true, 'large', 8, 3);
+-- current order
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (2, true, 'med', 9, 1);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (5, true, 'small', 9, 5);
+-- cart
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (1, true, 'large', 10, 2);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (3, true, 'med', 10, 3);
+INSERT INTO customizations (spiciness, hot, item_size, order_id, menu_item_id) VALUES (4, true, 'small', 10, 6);
