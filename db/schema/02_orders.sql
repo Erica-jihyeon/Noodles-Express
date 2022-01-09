b005f1da6ed56c863a577baa495c827f909add67
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-09 17:57:02 +0000

    edited db tables and seeds for test

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -4,6 +4,6 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   order_time TIMESTAMP NOT NULL,
-  pick_up_time TIMESTAMP NOT NULL,
+  pick_up_time TIMESTAMP,
   order_status VARCHAR(255)
 );
--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -4,4 +4,4 @@ INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (3, 
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (4, '2021-12-04T08:00:00.000Z', '2021-12-04T09:00:00.000Z', 'complete');
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (5, '2021-12-05T08:00:00.000Z', '2021-12-05T09:00:00.000Z', 'complete');
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (6, '2021-12-06T08:00:00.000Z', '2021-12-06T09:00:00.000Z', 'complete');
-INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2021-12-07T08:00:00.000Z', '2021-12-07T09:00:00.000Z', 'complete');
+INSERT INTO orders (user_id, order_time,order_status) VALUES (7, '2022-01-09T08:00:00.000Z', 'Preparing your meal');
