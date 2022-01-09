ea283b1fb9c2696feda2c58a5c6de3eeafdf1e01
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-09 22:05:30 +0000

    deleted unnecessary db files

--- db/schema/05_customizations.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS customizations CASCADE;
-
-CREATE TABLE customizations (
-  id SERIAL PRIMARY KEY NOT NULL,
-  spiciness INTEGER NOT NULL,
-  hot BOOLEAN NOT NULL,
-  item_size VARCHAR(10) NOT NULL,
-  menu_item_id INTEGER REFERENCES menu_items(id)
-);
--- db/seeds/05_customizations.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO customizations (spiciness, hot, item_size, menu_item_id) VALUES (5, true, 'large', 1);
-INSERT INTO customizations (spiciness, hot, item_size, menu_item_id) VALUES (3, true, 'small', 6);
-INSERT INTO customizations (spiciness, hot, item_size, menu_item_id) VALUES (1, false, 'large', 4);
-INSERT INTO customizations (spiciness, hot, item_size, menu_item_id) VALUES (3, true, 'small', 2);
-INSERT INTO customizations (spiciness, hot, item_size, menu_item_id) VALUES (1, false, 'med', 5);
-INSERT INTO customizations (spiciness, hot, item_size, menu_item_id) VALUES (2, true, 'large', 3);
-
-
