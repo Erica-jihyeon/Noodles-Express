23054f25840e84ebc686578942b3660f2b0c8d45
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-10 18:51:53 +0000

    added testing code to app.js for order page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 // Client facing scripts here
+$(() => {
+
+  const loadMenu = (category) => {
+    $.getJSON('/order/data')
+      .then((data) => {
+        console.log(`app.js`, data);
+      })
+  }
+  loadMenu('main');
+
+})
