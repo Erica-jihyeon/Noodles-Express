f88018bbfa9b1ea0238c917241247cb88516f819
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-10 02:04:28 +0000

    connected mypage routes with database and tested queries

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const { getCurrentOrdersDetails, getPrevOrdersDetails } = require('./mypage_database');
 const router  = express.Router();
 
-
 module.exports = (db) => {
+
+  const templateVars = {};
+
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+    //getCurrentOrdersDetails(db, req.session.user_id)
+    getCurrentOrdersDetails(db, 7)
       .then(data => {
-        const users = data.rows;
+        templateVars.currentOrder = data.currentOrder;
+        templateVars.currentOrderTotal = data.currentOrderTotal;
+        //return getPrevOrdersDetails(db, req.session.user_id)
+        return getPrevOrdersDetails(db, 7);
+      })
+      .then(data => {
+        templateVars.prevOrders = data.prevOrders;
+        templateVars.prevOrdersTotals = data.prevOrdersTotals;
+        console.log(templateVars);
         res.render('mypage');
       })
       .catch(err => {
