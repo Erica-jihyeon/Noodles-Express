c2effe110a47e71a9d9cfcfc1241c230a75e4a73
samsam3268 <samskleung@gmail.com>
2022-01-13 11:41:56 -0800

    fixed bug where mypage is showing wrong orders

--- db/seeds/02_orders.sql
+++ db/seeds/02_orders.sql
@@ -10,6 +10,6 @@ INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (6, 
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2021-12-09T08:00:00.000Z', '2021-12-09T09:00:00.000Z', 'complete');
 INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2021-12-10T08:00:00.000Z', '2021-12-10T09:00:00.000Z', 'complete');
 --current order
-INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2022-01-09T08:00:00.000Z', '2021-12-10T09:00:00.000Z', 'Preparing your meal');
+INSERT INTO orders (user_id, order_time, pick_up_time, order_status) VALUES (7, '2022-01-09T08:00:00.000Z', '2021-12-10T09:00:00.000Z', 'complete');
 --cart
 INSERT INTO orders (user_id) VALUES (7);
