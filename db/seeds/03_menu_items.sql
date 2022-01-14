cb596bcb4686cb66d1f0783683139b9899a5274f
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-14 01:57:17 +0000

    minor fixed on the seed file

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -1,25 +1,25 @@
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('pork ramen', '12', 'Chashu, Black Fungus, Egg, Green Onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641667836/noodles-express/ramen_uk5h1o.jpg',
+VALUES ('Pork Ramen', '12', 'Chashu, Black Fungus, Egg, Green Onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641667836/noodles-express/ramen_uk5h1o.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641667836/noodles-express/ramen_uk5h1o.jpg', 'main');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('vegetable ramen', '10', 'Vegetarian spicy mixed noodle with eggplant peperoncini, croquette, bamboo shoot and green onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671803/noodles-express/vegetable_ramen_da4nmo.jpg',
+VALUES ('Vegetable Ramen', '10', 'Vegetarian spicy mixed noodle with eggplant peperoncini, croquette, bamboo shoot and green onion', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671803/noodles-express/vegetable_ramen_da4nmo.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641671803/noodles-express/vegetable_ramen_da4nmo.jpg', 'main');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('mushroom fried noodle', '12', 'Freshly Made Hand pulled noodles, mushrooms, onion and carrots', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641671802/noodles-express/mushroom_fried_noodle_zuduei.jpg',
+VALUES ('Mushroom Fried Noodle', '12', 'Freshly Made Hand pulled noodles, mushrooms, onion and carrots', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641671802/noodles-express/mushroom_fried_noodle_zuduei.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_200/v1641671802/noodles-express/mushroom_fried_noodle_zuduei.jpg', 'main');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('coke', '3', 'Coke with ice', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671780/noodles-express/coke_hpel5p.jpg',
+VALUES ('Coke', '3', 'Coke with ice', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1641671780/noodles-express/coke_hpel5p.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641671780/noodles-express/coke_hpel5p.jpg', 'drink');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('strawberry_crepes', '5', 'Fresh Strawberries and whipping cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641672491/noodles-express/strawberry_crepes_b6safy.jpg',
+VALUES ('Strawberry Crepes', '5', 'Fresh Strawberries and whipping cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641672491/noodles-express/strawberry_crepes_b6safy.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641672491/noodles-express/strawberry_crepes_b6safy.jpg', 'dessert');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('fried rolls', '8', 'Fresh fried rolls, cheese and onions', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641673046/noodles-express/fried_roll_to2sbh.jpg',
+VALUES ('Fried Rolls', '8', 'Fresh fried rolls, cheese and onions', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641673046/noodles-express/fried_roll_to2sbh.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641673046/noodles-express/fried_roll_to2sbh.jpg', 'appetizer');
 
 
@@ -29,11 +29,11 @@ VALUES ('fried rolls', '8', 'Fresh fried rolls, cheese and onions', 'https://res
 
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('French fries', '6', 'French fries with special sauce', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124094/noodles-express/french-fries_vaztso.jpg',
+VALUES ('French Fries', '6', 'French fries with special sauce', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124094/noodles-express/french-fries_vaztso.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124094/noodles-express/french-fries_vaztso.jpg', 'appetizer');
 
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
-VALUES ('Pumkin soup', '8', 'Pumking soup with whip cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124096/noodles-express/pumpkin-soup_eoiffi.jpg',
+VALUES ('Pumkin Soup', '8', 'Pumking soup with whip cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124096/noodles-express/pumpkin-soup_eoiffi.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124096/noodles-express/pumpkin-soup_eoiffi.jpg', 'appetizer');
 
 
