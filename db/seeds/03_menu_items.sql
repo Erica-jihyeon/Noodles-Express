55c3427ac7a9cbbea3169d51e2fa82dd108e7868
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-14 01:55:21 +0000

    added more menu

--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -21,3 +21,47 @@ VALUES ('strawberry_crepes', '5', 'Fresh Strawberries and whipping cream', 'http
 INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
 VALUES ('fried rolls', '8', 'Fresh fried rolls, cheese and onions', 'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_1280/v1641673046/noodles-express/fried_roll_to2sbh.jpg',
 'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1641673046/noodles-express/fried_roll_to2sbh.jpg', 'appetizer');
+
+
+
+
+
+
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('French fries', '6', 'French fries with special sauce', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124094/noodles-express/french-fries_vaztso.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124094/noodles-express/french-fries_vaztso.jpg', 'appetizer');
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Pumkin soup', '8', 'Pumking soup with whip cream', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124096/noodles-express/pumpkin-soup_eoiffi.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124096/noodles-express/pumpkin-soup_eoiffi.jpg', 'appetizer');
+
+
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Fresh Lime Water', '3', 'Our fresh lime water with mint', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124360/noodles-express/lime_water_edgfby.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124360/noodles-express/lime_water_edgfby.jpg', 'drink');
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Special Tea', '3', 'Our special tea for you!', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124362/noodles-express/special_tea_sqtmfp.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124362/noodles-express/special_tea_sqtmfp.jpg', 'drink');
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Strawberry Smoothie', '4', 'Fresh strawberry smoothie', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124365/noodles-express/strawberry_vtpfwv.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124365/noodles-express/strawberry_vtpfwv.jpg', 'drink');
+
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Tiramisu', '5', 'Coffee-flavoured Italian dessert', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124787/noodles-express/Tiramisu_f9owdf.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124787/noodles-express/Tiramisu_f9owdf.jpg', 'dessert');
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Strawberry Tart', '7', 'Our fresh strawberry tart', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124784/noodles-express/tart-strawberry_uuwueb.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_scale,w_200/v1642124784/noodles-express/tart-strawberry_uuwueb.jpg', 'dessert');
+
+INSERT INTO menu_items (item_name, price, description, image_url, thumbnail_url, category)
+VALUES ('Berry Berry Yogurt', '5', 'Strawberry, blackberry and grains', 'https://res.cloudinary.com/drdbamx1y/image/upload/v1642124781/noodles-express/berry_yogurt_n2l5sn.jpg',
+'https://res.cloudinary.com/drdbamx1y/image/upload/c_thumb,w_200,g_face/v1642124365/noodles-express/strawberry_vtpfwv.jpg', 'dessert');
+
+
+
